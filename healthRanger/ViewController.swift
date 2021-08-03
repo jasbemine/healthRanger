affa1ba28a9a632d965948e8fbfb7324b6997d7d
jasmine <w.jasmine.j@gmail.com>
2021-08-03 14:40:31 -0700

    Initial Commit

---
+++ healthRanger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  healthRanger
+//
+//  Created by Jasmine  on 8/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
